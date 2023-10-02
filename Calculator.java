e479a9b2f1cbf528d0cfa744753180180781b5b6
sam <samakman888@gmail.com>
2023-10-02 23:36:51 +0800

    Added in files

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Calculator
+{
+    public static void main(String [] args)
+    {
+        Scanner myObj = new Scanner(System.in); 
+        System.out.println("Please enter your first number: ");
+        
+        int value1 = myObj.nextInt();
+
+        System.out.println("Please enter your second number: ");
+        
+        int value2 = myObj.nextInt();
+
+        int result = value1 + value2;
+
+        System.out.println("Here is the result: " + result);
+
+        myObj.close();
+
+
+
+
+    }
+}
---
+++ Calculator.py
@@ -0,0 +1,11 @@
+
+def main():
+    value1 = int(input("Please enter your first number: "))
+    value2 = int(input("Please enter your second number: "))
+
+    result = value1 + value2
+    print("This is your final value: " + str(result))
+
+
+if __name__ == "__main__":
+    main()
